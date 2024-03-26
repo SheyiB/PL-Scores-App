7d2a5b428b78ed938974e4a248cb06ffc7cfe56b
Elijah Banjo <elijahbanjo@gmail.com>
2024-03-26 22:16:14 +0100

    Chore:COnfigurations

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const tsconfig = require('./tsconfig.json');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  modulePaths: [tsconfig.compilerOptions.baseUrl], // Use tsconfig.compilerOptions.baseUrl instead
+
+};
